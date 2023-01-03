004d2a8c014af53e311a42b03fcdaa0510e8659a
charronadobe <74058178+charronadobe@users.noreply.github.com>
2023-01-02 23:50:39 -0500

    package-info.java update version

--- core/src/main/java/com/adobe/aem/guides/wknd/core/models/package-info.java
+++ core/src/main/java/com/adobe/aem/guides/wknd/core/models/package-info.java
@@ -13,7 +13,6 @@
  *  See the License for the specific language governing permissions and
  *  limitations under the License.
  */
-//@Version("2.2.0")
 /** */
-@org.osgi.annotation.versioning.Version("2.1.0")
+@org.osgi.annotation.versioning.Version("2.2.0")
 package com.adobe.aem.guides.wknd.core.models;
