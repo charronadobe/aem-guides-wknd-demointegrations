184165c93b6791d7e7ead70bd783b59d8a6362ad
charronadobe <74058178+charronadobe@users.noreply.github.com>
2025-05-02 13:18:39 -0400

    add custom confrag files to apps/wknd

---
+++ ui.apps/src/main/content/jcr_root/apps/wknd/components/contentfragment/js/content.js
@@ -0,0 +1,16 @@
+use(function () {
+  try {
+    const allowImageTypes = ["jpg", "jpeg", "gif", "png"];
+    const content = String(this.content);
+    var output = content;
+    const contentExtension = content.split(".").pop().toLowerCase();
+
+    if (allowImageTypes.indexOf(contentExtension) > -1) {
+      output = '<img src="' + content + '" />';
+    }
+  } catch (error) {
+    return;
+  }
+
+  return output;
+});
